4b1205b27751f6a5c510bf136c51666805c8fbef
mateo soria <matesoria28@gmail.com>
2026-04-20 14:01:44 -0300

    20/04

---
+++ PryEDSoriaM/clsCola.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PryEDSoriaM
+{
+    internal class clsCola
+    {
+        //campos de la clase
+        private clsNodo pri;
+        private clsNodo ult;
+
+        //propiedades de la clase
+        public clsNodo Primero
+        {
+            get { return pri; }
+            set { pri = value; }
+        }
+        public clsNodo Ultimo
+        {
+            get { return ult; }
+            set { ult = value; }
+        }
+
+        public void Agregar(clsNodo nuevo)
+        {
+            if (Primero == null)
+            {
+                Primero = nuevo;
+                Ultimo = nuevo;
+            }
+            else
+            {
+                Ultimo.Sig = nuevo;
+                Ultimo = nuevo;
+            }
+        }
+
+        public void Eliminar()
+        {
+            if (Primero == Ultimo)
+            {
+                Primero = null;
+                Ultimo = null;
+            }
+            else
+            { 
+                Primero = Primero.Sig;
+            }
+        }
+        public void Recorrer(DataGridView Grilla)
+        {
+            clsNodo aux = Primero;
+            Grilla.Rows.Clear();
+            while (aux != null)
+            {
+                Grilla.Rows.Add(aux.Cod, aux.Nom, aux.Tra);
+                aux = aux.Sig;
+            }
+        }
+        public void Recorrer(ListBox Lista)
+        {
+            clsNodo aux = Primero;
+            Lista.Items.Clear();
+            while (aux != null)
+            {
+                Lista.Items.Add(aux.Cod);
+                aux = aux.Sig;
+            }
+        }
+        public void Recorrer(ComboBox Combo)
+        {
+            clsNodo aux = Primero;
+            Combo.Items.Clear();
+            while (aux != null)
+            {
+                Combo.Items.Add(aux.Nom);
+                aux = aux.Sig;
+            }
+        }
+
+
+
+    }
+}
